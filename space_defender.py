7991678431e0d87d8177d2679dbc5df16731b8a0
Jesse224 <jeze363@gmail.com>
2025-05-29 20:58:15 +0300

    lisatty vihollisia

--- space_defender.py
+++ space_defender.py
@@ -12,7 +12,8 @@ pygame.display.set_caption("Space Defender")
 # Värit
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)  # Lisätty punainen väri ammuksille
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)  # Lisätty sininen väri ammuksille
 
 # Pelaajan alus (kolmion muoto, terävä kärki ylöspäin)
 player_points = [(WIDTH // 2, HEIGHT - 80), (WIDTH // 2 - 25, HEIGHT - 50), (WIDTH // 2 + 25, HEIGHT - 50)]
@@ -23,6 +24,20 @@ bullets = []
 bullet_speed = 7
 bullet_cooldown = 250  # Aika millisekunteina ammusten välillä
 last_shot_time = pygame.time.get_ticks()  # Viimeisimmän ammuksen aika
+
+# Viholliset
+enemies = []
+enemy_speed = 2
+enemy_spawn_cooldown = 1000  # Aika millisekunteina vihollisten välillä
+last_enemy_spawn_time = pygame.time.get_ticks()
+
+
+def spawn_enemy():
+    """Luo uuden vihollisen satunnaiseen paikkaan yläreunassa."""
+    enemy_x = random.randint(0, WIDTH - 30)  # Vihollisen leveys on 30
+    enemy_y = 0
+    enemies.append([enemy_x, enemy_y])
+
 
 # Pelisilmukka
 running = True
@@ -81,19 +96,33 @@ while running:
             for i in range(3):
                 player_points[i] = (player_points[i][0], player_points[i][1] + player_speed)
 
-    # Taustan piirto
+    # Vihollisten luominen
+    if current_time - last_enemy_spawn_time > enemy_spawn_cooldown:
+        spawn_enemy()
+        last_enemy_spawn_time = current_time
+
+    # Vihollisten liikkuminen
+    for enemy in enemies:
+        enemy[1] += enemy_speed
+        if enemy[1] > HEIGHT:  # Poista viholliset, jotka menevät ruudun ulkopuolelle
+            enemies.remove(enemy)
+
+    # Taustan piirto (SIIRRETTY YLÖS!)
     screen.fill(BLACK)
 
     # Ammusten liikkuminen ja piirto
     for bullet in bullets:
         bullet[1] -= bullet_speed  # Liikuta ammusta ylöspäin
-        pygame.draw.circle(screen, RED, (bullet[0], bullet[1]), 5)  # Piirrä ammus
+        pygame.draw.circle(screen, BLUE, (bullet[0], bullet[1]), 5)  # Piirrä ammus sinisenä
         if bullet[1] < 0:  # Poista ammukset, jotka menevät ruudun ulkopuolelle
             bullets.remove(bullet)
 
-
     # Pelaajan aluksen piirto kolmiona
     pygame.draw.polygon(screen, WHITE, player_points)
+
+    # Vihollisten piirto
+    for enemy in enemies:
+        pygame.draw.rect(screen, RED, (enemy[0], enemy[1], 30, 30))  # Piirrä vihollinen punaisena neliönä
 
     pygame.display.flip()
     pygame.time.Clock().tick(60)
